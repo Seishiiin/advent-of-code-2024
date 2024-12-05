432c83e206c900434d0d82fa5c9b2d8148b1cd0f
seishiiin <gabinhalloss@gmail.com>
2024-12-06 00:55:00 +0100

    day 6

--- advent_6.py
+++ advent_6.py
@@ -1,3 +1,9 @@
 # Description : Advent of Code 2020 - Day 6
 # Trouve toutes les paires d'éléments dans une liste qui, additionnés, donnent une somme spécifique.
 # [1, 2, 3, 4, 5, -1] et 4 => (1, 3), (2, 2), (3, 1), (-1, 5)
+
+def find_pair_oneline(liste, somme) -> list :
+    return [f"({i}, {somme-i})" for i in liste if somme - i in liste]
+
+print(find_pair_oneline([1, 2, 3, 4, 5, -1], 4)) # => [(1, 3), (2, 2), (3, 1), (-1, 5)]
+print(find_pair_oneline([1, 2, 3, 4, 5, -1], 5)) # => [(1, 4), (2, 3), (3, 2), (4, 1), (-1, 6)]
