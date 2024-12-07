a1e65ed48ca5fb2f077fd220ab938291517b1d32
seishiiin <gabinhalloss@gmail.com>
2024-12-07 17:14:49 +0100

    day 7

--- advent_7.py
+++ advent_7.py
@@ -1,3 +1,9 @@
 # Description : Advent of Code 2024 - Day 7
 # Vérifie si une chaîne donnée est un palindrome (insensible à la casse et aux espaces).
 # Exemple : "kayak" donne True et "hello" donne False.
+
+def is_palindrome(string) -> bool :
+    return string.lower().replace(" ", "") == string[::-1].lower().replace(" ", "")
+
+print(is_palindrome("Engage le jeu que je le gagne")) # => True
+print(is_palindrome("Salut les amis")) # => False
