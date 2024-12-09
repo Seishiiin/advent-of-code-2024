b33f9dd4be69562bb50c1f85882621aa6a9ed25b
seishiiin <gabinhalloss@gmail.com>
2024-12-09 10:14:21 +0100

    day 8 & 9

--- advent_8.py
+++ advent_8.py
@@ -1,3 +1,10 @@
 # Description : Advent of code 2024 - Day 8
 # Crée une liste de dictionnaires à partir d'un fichier texte formaté comme un tableau CSV simple (colonnes séparées par des virgules).
 # Exemple : "nom,age\nAlice,30\nBob,25\nCharlie,35" donne [{'nom': 'Alice', 'age': '30'}, {'nom': 'Bob', 'age': '25'}, {'nom': 'Charlie', 'age': '35'}].
+
+def csv_to_dicts(filename):
+    with open(filename) as f:
+        lines = f.read().splitlines()
+    return [dict(zip(lines[0].split(','), line.split(','))) for line in lines[1:]]
+
+print(csv_to_dicts('data.csv')) # [{'nom': 'Alice', 'age': '30'}, {'nom': 'Bob', 'age': '25'}, {'nom': 'Charlie', 'age': '35'}]
--- advent_9.py
+++ advent_9.py
@@ -1,3 +1,22 @@
 # Description : Advent of Code 2024 - Day 9
 # Écris un générateur de mots de passe aléatoires respectant certains critères : longueur minimale, majuscules, chiffres, caractères spéciaux.
 # Exemple : generate_password(8, True, True, True) donne "aB3@cdEf".
+
+import random
+
+def generate_password(length, uppercase, digits, special_chars):
+    valid_chars = []
+    password = ""
+
+    valid_chars += [chr(i) for i in range(65, 91)] if uppercase else []
+    valid_chars += [chr(i) for i in range(48, 58)] if digits else []
+    valid_chars += [chr(i) for i in range(33, 48)] if special_chars else []
+
+
+    for i in range(length):
+        password += random.choice(valid_chars)
+
+    return password
+
+print(generate_password(8, True, True, True)) # aB3@cdEf
+print(generate_password(20, True, True, False)) # aB3cdEf
